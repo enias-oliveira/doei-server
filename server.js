3660d35b035d488a8fee72269283e421c085aefe
Enias Oliveira <eniasoliveira27@gmail.com>
2021-01-13 19:05:00 -0300

    fix: ajusts server to work on heroku

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 4000;
+var cors = require('cors');
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
 
-server.use(middlewares);
-server.use(router);
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
+const port = process.env.PORT || 3001;
 
-server.listen(port);
+app.db = router.db;
+
+const rules = auth.rewriter({
+  "/users*": "/600/users$1",
+  "/campaigns*" : "/644/campaigns$1",
+    "/donations*" : "/640/donations$1"
+});
+
+app.use(cors());
+app.use(rules);
+app.use(auth);
+app.use(router);
+app.listen(port);
+
+console.log("Server is running on port:", port);
