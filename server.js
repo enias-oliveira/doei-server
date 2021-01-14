8b9aff2992ee0272eefbebd62cde2ce79cc17fb0
Enias Oliveira <eniasoliveira27@gmail.com>
2021-01-14 17:15:15 -0300

    feat: creates NGO endpoint

--- server.js
+++ server.js
@@ -9,9 +9,11 @@ const port = process.env.PORT || 3001;
 app.db = router.db;
 
 const rules = auth.rewriter({
-  "/users*": "/600/users$1",
+  "/users*": "/644/users$1",
   "/campaigns*" : "/644/campaigns$1",
-    "/donations*" : "/640/donations$1"
+    "/donations*" : "/640/donations$1",
+  "/ngos" : "/644/users?ngo=true",
+  "/ngos/:id" : "/644/users?ngo=true&id=:id"
 });
 
 app.use(cors());
